f938b69def63928129f4258c857501b93e6d769a
Christian Macht <c.macht@schule.bremen.de>
2021-04-25 00:57:25 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,33 +7,38 @@ homer4 = main.Homer()
 homer5 = main.Homer()
 
 
-def test_class_homer():
-    assert homer.name == 'Homer'
-    assert homer.city == 'Springfield'
-    assert homer.age == 39
+class TestBasic:
+
+    def test_class_homer(self):
+        assert homer.name == 'Homer'
+        assert homer.city == 'Springfield'
+        assert homer.age == 39
+
+    def test_class_homer_do_chores(self):
+        method = getattr(homer, 'do_chores')
+        assert callable(method)
+
+    def test_hammock_returns_list(self):
+        result = main.magic_hammock(homer)
+        assert isinstance(result, list)
+        assert len(result) == 2
+
+    def test_hammock_returns_clone(self):
+        result = main.magic_hammock(homer)
+        assert isinstance(result[0], main.Homer)
+        assert result[0] is homer
+        assert isinstance(result[1], main.Homer)
+
+    def test_clone_chores(self):
+        original, clone = main.magic_hammock(homer)
+        assert original.do_chore() == 'Nope!'
+        assert clone.do_chore() == 'Ok!'
 
 
-def test_hammock_returns_list():
-    result = main.magic_hammock(homer)
-    assert isinstance(result, list)
-    assert len(result) == 2
+class TestBonus:
 
-
-def test_hammock_returns_clone():
-    result = main.magic_hammock(homer)
-    assert isinstance(result[0], main.Homer)
-    assert result[0] is homer
-    assert isinstance(result[1], main.Homer)
-
-
-def test_hammock_returns_multiples():
-    result = main.magic_hammock(homer, homer2, homer3, homer4, homer5)
-    assert len(result) == 10
-    for element in result:
-        assert isinstance(element, main.Homer)
-
-
-def test_clone_chores():
-    original, clone = main.magic_hammock(homer)
-    assert original.do_chore() == 'Nope!'
-    assert clone.do_chore() == 'Ok!'
+    def test_hammock_returns_multiples(self):
+        result = main.magic_hammock(homer, homer2, homer3, homer4, homer5)
+        assert len(result) == 10
+        for element in result:
+            assert isinstance(element, main.Homer)
